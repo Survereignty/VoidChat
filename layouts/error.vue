24a15c124233d29690690d86fc8f3f314c39fbb1
Survereignty <survereignty@gmail.com>
2019-10-06 21:35:59 +1100

    ready

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <v-content>
+            <v-container fluid>
+                <h1>404</h1>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
